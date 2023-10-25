cc230bdfe80de2a4ccc445d312daa717ff3a77c6
Nate Schutte <natecodenew@gmail.com>
2023-10-24 21:39:50 -0700

    update data gen

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -3,10 +3,14 @@
 import json
 import random
 import os
+import datetime
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 OUTPUT_DIR = '.data'
 N = 10
+MAX_DEPTH = 1
+MAX_ARRAY_LEN = 4
+DROP_CHANCE = 6
 
 def main():
     with open(f'{THIS_DIR}/objects.txt', 'r') as f:
@@ -22,41 +26,57 @@ def main():
     def _gen(obj, n=0):
         for k in [o.strip() for o in random.choices(objects, k=N)]:
             obj[k] = {}
+
         for field in obj.keys():
-            in_field = random.choice(fields).strip()
-            if in_field in obj[field]:
-                continue
-            obj[field][in_field] = {}
-            if n < 1 and random.randint(0, 1) == 0:
+            obj[field] = {}
+            if n < MAX_DEPTH and random.randint(0, 1) == 0:
                 if random.randint(0, 1) == 0:
-                    obj[field][in_field] = _gen(obj[field][in_field], n=n+1)
+                    obj[field] = _gen(obj[field], n=n+1)
                 else:
-                    obj[field][in_field] = [_gen({}, n=n+1)]
+                    obj[field] = [_gen({}, n=n+1)]
+        
         return obj
 
     def _fill(obj, n=0):
+        drop_q = []
         for k, v in obj.items():
             if isinstance(v, dict):
+                if random.randint(0, DROP_CHANCE) == 0:
+                    drop_q.append(k)
+                    continue
                 if len(v.keys()) > 0:
                     obj[k] = _fill(v, n=n+1)
                 else:
-                    if random.randint(0, 2) == 0:
-                        if random.randint(0, 1) == 0:
-                            obj[k] = random.randint(-999, 999)
-                        else:
-                            obj[k] = random.uniform(-999, 999)
-                    else:
+                    type = random.randint(0, 5)
+                    if type == 0:
+                        obj[k] = random.randint(-999, 999)
+                    elif type == 1:
+                        obj[k] = random.uniform(-999, 999)
+                    elif type == 2:
                         obj[k] = random.choice(values).strip()
+                    elif type == 3:
+                        obj[k] = random.randint(0, 1) == 0
+                    elif type == 4:
+                        obj[k] = None
+                    elif type == 5:
+                        if random.randint(0, 1) == 0:
+                            obj[k] = datetime.datetime.now().isoformat()
+                        else:
+                            obj[k] = datetime.datetime.now().time().isoformat()
             elif isinstance(v, list):
                 if len(v) > 0:
                     el = v[0]
-                    for _ in range(random.randint(1, 5)):
+                    for _ in range(random.randint(0, MAX_ARRAY_LEN)):
                         v.append(el.copy())
                     for i in range(len(v)):
                         v[i] = _fill(v[i], n=n+1)
 
                 else:
                     obj[k] = [_fill({}, n=n+1) for _ in range(random.randint(1, 5))]
+        
+        for k in drop_q:
+            del obj[k]
+
         return obj
     
     struct = _gen({})
@@ -66,7 +86,14 @@ def main():
         obj = _fill(obj)
 
         with open(f'{OUTPUT_DIR}/object_{i}.json', 'w') as f:
-            json.dump(obj, f, indent=4)
+            json.dump({
+                'data': obj,
+                'metadata': {
+                    'id': i,
+                    'name': f'Object {i}',
+                    'generated_at': datetime.datetime.now().isoformat(),
+                }
+            }, f, indent=4)
         
 
 if __name__ == '__main__':
