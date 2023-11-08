78e04712fae0be0932dd6f1c5b6a81ed873fcd1d
Nate Schutte <natecodenew@gmail.com>
2023-11-08 11:47:38 -0800

    reorg test briefly

--- test/code/parsers/test_json_parser.py
+++
@@ -1,5 +0,0 @@
-from tosser.parsers.common import JsonParser
-
-
-def test_import_parsers() -> None:
-    parser = JsonParser()
---
+++ test/schema/test_schema.py
@@ -0,0 +1,7 @@
+import pytest
+
+from tosser import Tosser
+
+def test_schema():
+    ts = Tosser()
+    assert True
